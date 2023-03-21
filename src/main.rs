041f701b7fdad6ffe8bb1703b8bc1f3e3afeb65e
mharv <mitch.k.harvey@gmail.com>
2023-03-21 18:30:23 +0000

    testing fs module from std library

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-
-
-
-
-
+use std::fs::{self, DirEntry, metadata};
+use std::io;
+use std::path::Path;
 
 fn main() {
     // check if folder named bookmarks exists.
+    //
+    let dirs = fs::read_dir(".").unwrap();
+
+    for dir in dirs {
+        if metadata(dir.as_ref().unwrap().path()).unwrap().is_dir() {
+            println!("{:?}", dir);
+        }
+    }
 
     // if not, create one.
 
@@ -16,10 +22,8 @@ fn main() {
     // convert data into filesystem after decryption
 
     // have following functionaility:
-    // - url 200 health check (request) 
+    // - url 200 health check (request)
     // - display urls in a folder
     // - move and delete urls.
     // - sync back to github profile
-    
-
 }
