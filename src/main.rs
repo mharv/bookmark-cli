25f5ecbda214ae4e3735cbe95a2d4f70ae39130b
mharv <mitch.k.harvey@gmail.com>
2023-03-21 18:00:20 +0000

    rough outline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+
+
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    // check if folder named bookmarks exists.
+
+    // if not, create one.
+
+    // do a git pull from a github repo.
+
+    // do auto merge.
+
+    // convert data into filesystem after decryption
+
+    // have following functionaility:
+    // - url 200 health check (request) 
+    // - display urls in a folder
+    // - move and delete urls.
+    // - sync back to github profile
+    
+
 }
